14e6f26378331e41e50617b54926438ab91552f0
Mohamed Baroudy <mohamed.baroudy@gizasystems.local>
2020-06-06 23:10:48 +0200

    week 2 first 2 problems

--- week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
+++ week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
@@ -8,10 +8,30 @@ public class Fibonacci {
     return calc_fib(n - 1) + calc_fib(n - 2);
   }
 
+  /** 
+   * this is mine function to return Fn with the temporary variable
+   * O(n)*/ 
+  private static long calc_fib_temp_method(int n){
+
+    if (n <= 1)
+      return n;
+    
+    long last2= 0;
+    long last1= 1;
+    long result =0;
+    for (int i = 1; i < n; i++) {
+      result = last1+last2;
+      last2 = last1;
+      last1 = result;
+    }
+    return result;
+  }
+
   public static void main(String args[]) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
 
-    System.out.println(calc_fib(n));
+    // System.out.println(calc_fib(n));
+    System.out.println(calc_fib_temp_method(n));
   }
 }
--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
@@ -16,11 +16,35 @@ public class FibonacciLastDigit {
 
         return current % 10;
     }
+
+
+    /**
+     * this is my implementation and it solvs the problem 
+     * 0,1,1,2,3,5,8,13,21,34,55,89,144,233.377
+     * try to add just the last number
+     * 0,1,1,2,3,5,8,3,1,4,5,9,4,3,7
+     * @param args
+     */
+     private static int getFibonacciLastDigiteffecient(int n) {
+         if (n<=1) {
+             return n;
+         }
+
+         int previous=0;
+         int current=0;
+
+         for (int i = 0; i < n-1; i++) {
+             int temp = previous;
+             previous = current;
+             current = (temp + current)%10;
+         }
+         return current;
+     }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int c = getFibonacciLastDigitNaive(n);
+        int c = getFibonacciLastDigiteffecient(n);
         System.out.println(c);
     }
 }
