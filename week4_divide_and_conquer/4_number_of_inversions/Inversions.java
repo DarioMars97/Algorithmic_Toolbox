92c0b80847809fa8b50556432c1c880b03ff62b6
Mohamed Baroudy <mohamed.baroudy@gizasystems.local>
2020-07-07 19:55:05 +0200

    number of inversions

--- week4_divide_and_conquer/4_number_of_inversions/Inversions.java
+++ week4_divide_and_conquer/4_number_of_inversions/Inversions.java
@@ -2,6 +2,47 @@ import java.util.*;
 
 public class Inversions {
 
+    public static long nuOfInversions = 0;
+
+    public static int[] merge(int[] leftArr, int[] rightArr) {
+        int leftCounter = 0, rightCounter = 0;
+        int[] mergedArray = new int[leftArr.length+rightArr.length];
+        while (leftCounter < leftArr.length || rightCounter < rightArr.length) {
+            if (leftCounter < leftArr.length && rightCounter < rightArr.length) {
+                if (leftArr[leftCounter] <= rightArr[rightCounter]) {
+                    mergedArray[leftCounter+rightCounter] = leftArr[leftCounter];
+                    leftCounter++;
+                }else {
+                    mergedArray[leftCounter+rightCounter] = rightArr[rightCounter];
+                    rightCounter++;
+
+                    // if first element in remaining right array > first element in remaining left array
+                    // then we use the remaining # of elements in leftArr which is 
+                    // leftArr.length - leftCounter
+                    nuOfInversions+= leftArr.length - leftCounter;
+                }   
+            } else if (leftCounter < leftArr.length && rightCounter == rightArr.length) {
+                mergedArray[leftCounter+rightCounter] = leftArr[leftCounter];
+                leftCounter++;
+            } else {
+                mergedArray[leftCounter+rightCounter] = rightArr[rightCounter];
+                rightCounter++;
+            }
+        }
+        return mergedArray;
+    }
+
+    public static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+        int[] left = Arrays.copyOfRange(arr, 0, arr.length/2);
+        int[] right = Arrays.copyOfRange(arr, arr.length/2, arr.length);
+        int[] leftArr = mergeSort(left);
+        int[] rightArr = mergeSort(right);
+        return merge(leftArr, rightArr);
+    }
+
     private static long getNumberOfInversions(int[] a, int[] b, int left, int right) {
         long numberOfInversions = 0;
         if (right <= left + 1) {
@@ -22,7 +63,9 @@ public class Inversions {
             a[i] = scanner.nextInt();
         }
         int[] b = new int[n];
-        System.out.println(getNumberOfInversions(a, b, 0, a.length));
+        // System.out.println(getNumberOfInversions(a, b, 0, a.length));
+        mergeSort(a);
+        System.out.println(nuOfInversions);
     }
 }
 
